e5a6d09fb69d84a6dbd3607f03749e13cc9a9e4b
sokunsamnang <sokunsamnang45@gmail.com>
2021-12-23 17:01:00 +0700

    changed api to production

--- lib/core/backend/api_service.dart
+++ lib/core/backend/api_service.dart
@@ -1,11 +1,11 @@
 class ApiService {
   // Production API
-  // static final url = 'https://api-hotspot.koompi.org/api';
+  static final url = 'https://api-hotspot.koompi.org/api';
   static final getAvatar = 'https://api-hotspot.koompi.org/uploads';
   static final notiImage = 'https://api-hotspot.koompi.org/uploads/noti';
 
   // Development API
-  static final url = 'https://api-hotspot-dev.koompi.org/api';
+  // static final url = 'https://api-hotspot-dev.koompi.org/api';
   // static final getAvatar = 'https://api-hotspot-dev.koompi.org/uploads';
   // static final notiImage = 'https://api-hotspot-dev.koompi.org/uploads/noti';
 }
